002500262674c23806dd8fa62e0232f877eeb4b8
Trevon Lamb <48192916+trevonlamb@users.noreply.github.com>
2020-12-28 01:04:50 -0500

    got access to yelp's API to turn into json repsonse

---
+++ app/src/main/java/com/example/foodmoodapp/Constants.java
@@ -0,0 +1,12 @@
+package com.example.foodmoodapp;
+
+
+
+
+public class Constants {
+    public static final String YELP_TOKEN = String.valueOf(R.string.YelpToken);
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=restaurants";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+}
+
+
---
+++ app/src/main/java/com/example/foodmoodapp/SearchActivity.java
@@ -0,0 +1,47 @@
+package com.example.foodmoodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class SearchActivity extends AppCompatActivity {
+    public static final String TAG = SearchActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        // need to get location
+        getRestaurants(location);
+    }
+
+
+    private void getRestaurants(String Location){
+        final YelpService yelpService = new YelpService();
+        yelpService.findRestaurants(Location, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+    }
+}
